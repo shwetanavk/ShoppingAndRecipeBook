6cdc6847b2c1cc8327bad6a5ea344c39507accc9
Shweta Navkar <shwetanavkar18@gmail.com>
2022-08-23 16:29:47 +0530

    added custom dropdown directive

--- Recipe-And-Shoping-App/src/app/app.module.ts
+++ Recipe-And-Shoping-App/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
